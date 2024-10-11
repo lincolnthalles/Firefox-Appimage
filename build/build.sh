d5e7cf467549ba674e7e69454de3a7f3c84e9d13
Lincoln Nogueira <lincolnthalles@users.noreply.github.com>
2024-10-11 16:59:52 -0300

    ci: tweak action

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export APPIMAGE_EXTRACT_AND_RUN=1
-export FIREFOX_PRODUCT="${FIREFOX_PRODUCT:-firefox-esr}"
+export FIREFOX_CHANNEL="${FIREFOX_CHANNEL:-firefox-esr}"
 export FIREFOX_OS="${FIREFOX_OS:-linux64}"
 export FIREFOX_LANG="${FIREFOX_LANG:-en-US}"
 export GITHUB_ENV="${GITHUB_ENV:-.GITHUB_ENV}"
@@ -12,12 +12,12 @@ rm -rf build/AppDir
 rm -rf build/firefox*
 mkdir -p build/src
 
-if [[ ! -f "build/src/${FIREFOX_PRODUCT}-${FIREFOX_LANG}.tar.bz2" ]]; then
-    echo "==> Downloading ${FIREFOX_PRODUCT}"
-    wget "https://download.mozilla.org/?product=${FIREFOX_PRODUCT}-latest-ssl&os=${FIREFOX_OS}&lang=${FIREFOX_LANG}" -O "build/src/${FIREFOX_PRODUCT}-${FIREFOX_LANG}.tar.bz2"
+if [[ ! -f "build/src/${FIREFOX_CHANNEL}-${FIREFOX_LANG}.tar.bz2" ]]; then
+    echo "==> Downloading ${FIREFOX_CHANNEL}"
+    wget "https://download.mozilla.org/?product=${FIREFOX_CHANNEL}-latest-ssl&os=${FIREFOX_OS}&lang=${FIREFOX_LANG}" -O "build/src/${FIREFOX_CHANNEL}-${FIREFOX_LANG}.tar.bz2"
 fi
 
-tar -xvf "build/src/${FIREFOX_PRODUCT}-${FIREFOX_LANG}.tar.bz2" -C build
+tar -xvf "build/src/${FIREFOX_CHANNEL}-${FIREFOX_LANG}.tar.bz2" -C build
 mv build/firefox* build/AppDir
 
 echo "==> Copying AppRun"
@@ -25,18 +25,22 @@ sed "s,FIREFOX_BIN_FILE,$(basename build/AppDir/firefox*-bin),g" <AppRun >build/
 chmod 755 build/AppDir/AppRun
 
 echo "==> Copying Firefox Desktop file"
-cp desktop/"${FIREFOX_PRODUCT}".desktop build/AppDir/.
+cp desktop/"${FIREFOX_CHANNEL}".desktop build/AppDir/.
 
 echo "==> Disable Auto Updates"
 cp -r distribution build/AppDir/.
 
-FIREFOX_ICON_NAME="$(grep 'Icon=' <desktop/"${FIREFOX_PRODUCT}".desktop | sed 's,Icon=,,g')"
+FIREFOX_ICON_NAME="$(grep 'Icon=' <desktop/"${FIREFOX_CHANNEL}".desktop | sed 's,Icon=,,g')"
 echo "==> Copying icon :: ${FIREFOX_ICON_NAME}"
 ln -sr build/AppDir/browser/chrome/icons/default/default128.png build/AppDir/"${FIREFOX_ICON_NAME}".png
 
+if ! [ -x "$(command -v desktop-file-validate)" ]; then
+    sudo apt-get -y install desktop-file-utils
+fi
+
 if ! [ -f "build/appimagetool" ]; then
     echo "==> Downloading appimagetool"
-    wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-$(uname -m).AppImage" -O build/appimagetool
+    wget "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$(uname -m).AppImage" -O build/appimagetool
     chmod +x build/appimagetool
 fi
 
@@ -64,17 +68,17 @@ case "$FIREFOX_OS" in
 esac
 export ARCH=FIREFOX_ARCH
 
-./build/appimagetool -n --comp gzip \
+./build/appimagetool -n --comp zstd \
     build/AppDir \
-    --updateinformation "gh-releases-zsync|$GH_USER|$GH_REPO|$FIREFOX_PRODUCT|$FIREFOX_PRODUCT*.AppImage.zsync" \
-    "${FIREFOX_PRODUCT}-${FIREFOX_VERSION}-${FIREFOX_ARCH}-${FIREFOX_LANG}".AppImage
+    --updateinformation "gh-releases-zsync|$GH_USER|$GH_REPO|$FIREFOX_CHANNEL|$FIREFOX_CHANNEL*.AppImage.zsync" \
+    "${FIREFOX_CHANNEL}-${FIREFOX_VERSION}-${FIREFOX_ARCH}-${FIREFOX_LANG}".AppImage
 
 mkdir -p dist
-mv "$FIREFOX_PRODUCT"*.AppImage* dist/.
-echo "==> Done, saved $(realpath dist/"$FIREFOX_PRODUCT"*.AppImage)"
+mv "$FIREFOX_CHANNEL"*.AppImage* dist/.
+echo "==> Done, saved $(realpath dist/"$FIREFOX_CHANNEL"*.AppImage)"
 
 RELEASE_NOTES_URL="https://www.mozilla.org/firefox/$FIREFOX_VERSION"
-if [[ "$FIREFOX_PRODUCT" == "firefox-devedition" ]] || [[ "$FIREFOX_PRODUCT" == "firefox-beta" ]]; then
+if [[ "$FIREFOX_CHANNEL" == "firefox-devedition" ]] || [[ "$FIREFOX_CHANNEL" == "firefox-beta" ]]; then
     RELEASE_NOTES_URL="${RELEASE_NOTES_URL}beta"
 fi
 RELEASE_NOTES_URL="${RELEASE_NOTES_URL}/releasenotes/"
@@ -85,3 +89,5 @@ echo "==> GitHub Actions "
     echo "FIREFOX_BUILD_ID=$FIREFOX_BUILD_ID"
     echo "RELEASE_NOTES_URL=$RELEASE_NOTES_URL"
 } >>"$GITHUB_ENV"
+
+echo "$FIREFOX_VERSION.r$FIREFOX_BUILD_ID"
